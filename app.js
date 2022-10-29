b106ab3347b0d6046a2c5d394697b7e609fe59e9
Ivan Maras <64895322+Adonael@users.noreply.github.com>
2022-10-29 11:33:36 +0200

    Passing data from web to server(ejs, node)

--- app.js
+++ app.js
@@ -1,42 +1,32 @@
 const express=require("express");
-const https=require("https");
 const bodyParser=require("body-parser");
 
 const app=express();
 app.use(bodyParser.urlencoded({extended:true}));
+app.set("view engine", "ejs");
+
+var items=["Buy Food", "Cook Food", "Eat Food"];
 
 app.get("/", function(req, res){
-res.sendFile(__dirname+"/index.html");
-});
+    var today=new Date();
+    var options={
+        weekday: "long",
+        day:"numeric",
+        month:"long"
+    };
 
-app.post("/", function(req, res){
-
-    const query=req.body.cityName;
-    const apiKey="c70ee0da76caf09c5d9381f4a37c328f";
-    const unit="metric";
-    const url="https://api.openweathermap.org/data/2.5/weather?q="+ query +"&appid="+ apiKey +"&units="+unit;
-    https.get(url, function(response){
-        console.log(response.statusCode);
-
-    response.on("data", function(data){
-        const weatherData=JSON.parse(data);
-        const temp= weatherData.main.temp;
-        const weatherDescription=weatherData.weather[0].description;
-        const icon=weatherData.weather[0].icon;
-        const imageURL="http://openweathermap.org/img/wn/"+icon+"@2x.png"
-        res.write("<p> The weather in "+query+" is currently:"+weatherDescription+"</p>")
-        res.write("<h1>The temperature in "+query+" is "+temp+" degrees Celsius.</h1>");
-        res.write("<img src="+imageURL+">");
-        res.send();
-        });
-});
-});
+    var day=today.toLocaleDateString("en-US", options);
     
+        res.render("list", {kindOfDay: day, newListItems: items});        
+});
 
+app.post("/",function(req, res){
+    var item=req.body.task;
 
+    items.push(item);
 
-
-
+    res.redirect("/");
+});
 
 app.listen(3000, function(){
     console.log("Server is running on port 3000");
